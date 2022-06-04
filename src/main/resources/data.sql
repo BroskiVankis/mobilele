f95ecc83c45867eedffaf4d1923cccba39a1c06f
BroskiVankis <ivanov.vankis@gmail.com>
2022-06-04 16:49:58 +0300

    Initial login/registration

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+-- some test users
+INSERT INTO users (id, email, first_name, last_name, image_url, is_active, password)
+VALUES (1, 'ivanov.vankis@gmail.com', 'Ivan', 'Ivanov', null, 1, 'c8537422db0f64451fe81a55c89386b346270d059105e2d4a263edd2003e6d87a692ab319de515f5');
